8462e31f25c75c33e93871dcda5ceba16da38ad8
peterhhchan <peterhhchan@gmail.com>
2021-12-02 02:50:44 -0500

    Day 2 Refactor

--- src/day2.rs
+++ src/day2.rs
@@ -1,40 +1,39 @@
 use std::fs;
 
-pub fn part1() -> i32 {
-    let readings = fs::read_to_string("data/day2.txt").unwrap();
-
-    let res = readings
+fn data() -> Vec<(String, i32)> {
+    fs::read_to_string("data/day2.txt")
+        .unwrap()
         .lines()
         .map(|s| {
             let mut line = s.split(' ');
-            (line.next().unwrap(), line.next().unwrap().parse().unwrap())
+            (
+                line.next().unwrap().to_owned(),
+                line.next().unwrap().parse().unwrap(),
+            )
         })
-        .fold(
-            (0, 0),
-            |(h, d), (direction, v): (&str, i32)| match direction {
-                "forward" => (h + v, d),
-                "down" => (h, d - v),
-                "up" => (h, d + v),
-                _ => (h, d),
-            },
-        );
+        .collect()
+}
+
+pub fn part1() -> i32 {
+    let res = data().iter().fold(
+        (0, 0),
+        |(h, d), (direction, v): &(String, i32)| match direction.as_str() {
+            "forward" => (h + v, d),
+            "down" => (h, d - v),
+            "up" => (h, d + v),
+            _ => (h, d),
+        },
+    );
 
     println! {"{:?}", res.0 * res.1};
     res.0 * res.1
 }
 
 pub fn part2() -> i32 {
-    let readings = fs::read_to_string("data/day2.txt").unwrap();
-
-    let res = readings
-        .lines()
-        .map(|s| {
-            let mut line = s.split(' ');
-            (line.next().unwrap(), line.next().unwrap().parse().unwrap())
-        })
-        .fold(
+    let res =
+        data().iter().fold(
             (0, 0, 0),
-            |(h, d, aim), (direction, v): (&str, i32)| match direction {
+            |(h, d, aim), (command, v): &(String, i32)| match command.as_str() {
                 "forward" => (h + v, d + aim * v, aim),
                 "down" => (h, d, aim - v),
                 "up" => (h, d, aim + v),
