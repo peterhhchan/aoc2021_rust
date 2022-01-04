d3bc8c5b8b99c90c585b9e51cea3e833775b993d
peterhhchan <peterhhchan@gmail.com>
2022-01-04 10:03:19 -0500

    Day 25 code cleanup

--- src/day25.rs
+++ src/day25.rs
@@ -6,14 +6,17 @@ use std::str;
 // 47.88ms in release
 pub fn part1() {
     let trench = fs::read_to_string("data/day25.txt").unwrap();
-    let mut board: Vec<Vec<u8>> = trench.lines().map(|s| s.as_bytes().to_vec()).collect();
+    // Can use as_bytes() instead of chars() here, would require 1
+    // byte instead of 4, however, performance remains unchanged for
+    // this specific example.
+    let mut board: Vec<Vec<char>> = trench.lines().map(|s| s.chars().collect()).collect();
     let mut steps = 0;
     let width = board[0].len();
     let height = board.len();
 
-    let EMPTY = b'.';
-    let RIGHT = b'>';
-    let DOWN = b'v';
+    let empty = '.';
+    let right = '>';
+    let down = 'v';
 
     let mut changed = true;
     while changed {
@@ -21,41 +24,41 @@ pub fn part1() {
 
         changed = false;
         for l in board.iter_mut() {
-            let last: u8 = l[width - 1];
+            let last: char = l[width - 1];
             let prev = l.clone();
             for i in 0..l.len() - 1 {
                 // Could use (i + 1 % width) here, but its almost 2x slower
-                if prev[i + 1] == EMPTY && prev[i] == RIGHT {
-                    l[i + 1] = RIGHT;
-                    l[i] = EMPTY;
+                if prev[i + 1] == empty && prev[i] == right {
+                    l[i + 1] = right;
+                    l[i] = empty;
                     changed = true;
                 }
             }
-            if last == RIGHT && prev[0] == EMPTY {
-                l[width - 1] = EMPTY;
-                l[0] = RIGHT;
+            if last == right && prev[0] == empty {
+                l[width - 1] = empty;
+                l[0] = right;
                 changed = true;
             }
         }
 
         for j in 0..width {
-            let last: u8 = board[height - 1][j];
+            let last: char = board[height - 1][j];
 
-            let mut prev = vec![0; height];
+            let mut prev: Vec<char> = vec![0 as char; height];
             for k in 0..height {
                 prev[k] = board[k][j];
             }
 
             for k in 0..height - 1 {
-                if prev[k + 1] == EMPTY && prev[k] == DOWN {
-                    board[k][j] = EMPTY;
-                    board[k + 1][j] = DOWN;
+                if prev[k + 1] == empty && prev[k] == down {
+                    board[k][j] = empty;
+                    board[k + 1][j] = down;
                     changed = true;
                 }
             }
-            if last == DOWN && prev[0] == EMPTY {
-                board[height - 1][j] = EMPTY;
-                board[0][j] = DOWN;
+            if last == down && prev[0] == empty {
+                board[height - 1][j] = empty;
+                board[0][j] = down;
                 changed = true;
             }
         }
